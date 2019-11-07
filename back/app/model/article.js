a41e28320690d7d4dedd613846e549bde5e466ec
kewins86 <114571429@qq.com>
2019-11-07 11:47:57 +0800

    格式化

--- back/app/model/article.js
+++ back/app/model/article.js
@@ -6,18 +6,18 @@ module.exports = app=>{
 
 	// 定义一个用户模型
 	const ArticleSchema = new Schema({
-		__v:{type:Number, select:false},
-		title: {type:String, required:true},
-		article: {type:String, required:true, select:false},
-		article_html: {type:String, required:true},
+		__v: { type:Number, select:false },
+		title: { type:String, required:true },
+		article: { type:String, required:true, select:false },
+		article_html: { type:String, required:true },
 		author: {
 			type:Schema.Types.ObjectId,
 			ref: 'User',
 			required:true
 		},
-		views:{type:Number, required:false, default: 1},
-		like:{type:Number, required:false, default: 0},
-		dislike:{type:Number, required:false, default: 0}
+		views: { type:Number, required:false, default: 1 },
+		like: { type:Number, required:false, default: 0 },
+		dislike: { type:Number, required:false, default: 0 }
 	})
 	return mongoose.model('Article', ArticleSchema)
 }
